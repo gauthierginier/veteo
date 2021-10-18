21f8e57148288b2166cbbbddfca8227a7125476c
gauthierginier <gauthier.ginier@gmail.com>
2021-10-18 03:05:26 +0200

    schedule refresh of xml data

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from functions.getVelomaggData import getVeloMaggData
 from functions.parseXmlData import parseXmlData
+import schedule
+import time
 
-getVeloMaggData()
-velomaggData = parseXmlData("./velomaggData.xml")
-for i in velomaggData:
-    print(i)
+def getData():
+    getVeloMaggData()
+    velomaggData = parseXmlData("./velomaggData.xml")
+    for i in velomaggData:
+        print(i)
+schedule.every(1).minutes.do(getData)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
