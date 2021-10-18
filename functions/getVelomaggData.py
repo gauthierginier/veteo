fa9266e44268c9cb0bbb7601643141b80cdfe091
gauthierginier <gauthier.ginier@gmail.com>
2021-10-18 02:17:00 +0200

    download velomagg xml datas from openData3M

---
+++ functions/getVelomaggData.py
@@ -0,0 +1,8 @@
+import requests
+
+def getVeloMaggData():
+    velomaggXmlUrl = 'https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_VELOMAG.xml'
+    getVelomaggXml = requests.get(velomaggXmlUrl, allow_redirects=True)
+    open('velomaggData.xml', 'wb').write(getVelomaggXml.content)
+    return True
+getVeloMaggData()
