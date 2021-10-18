5774de6b9b167dd12206ee026e69fb6a54234182
gauthierginier <gauthier.ginier@gmail.com>
2021-10-19 00:39:45 +0200

    add logging startegy

--- functions/getVelomaggData.py
+++ functions/getVelomaggData.py
@@ -1,7 +1,9 @@
 import requests
+import logging
 
 def getVeloMaggData():
     velomaggXmlUrl = 'https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_VELOMAG.xml'
     getVelomaggXml = requests.get(velomaggXmlUrl, allow_redirects=True)
     open('velomaggData.xml', 'wb').write(getVelomaggXml.content)
+    logging.debug("xml file was downloaded successfuly")
     return True
--- main.py
+++ main.py
@@ -2,13 +2,26 @@ from functions.getVelomaggData import getVeloMaggData
 from functions.parseXmlData import parseXmlData
 import schedule
 import time
+import logging
+from logging.handlers import RotatingFileHandler
+
+logging.basicConfig(
+        handlers=[RotatingFileHandler('./logs/logs.log', maxBytes=100000, backupCount=10)],
+        level=logging.DEBUG,
+        format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+        datefmt='%Y-%m-%dT%H:%M:%S')
+
 
 def getData():
     getVeloMaggData()
     velomaggData = parseXmlData("./velomaggData.xml")
     for i in velomaggData:
         print(i)
+
+
+getData()
 schedule.every(1).minutes.do(getData)
+
 while True:
     schedule.run_pending()
     time.sleep(1)
